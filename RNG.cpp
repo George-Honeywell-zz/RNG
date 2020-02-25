e5c790a5d9ee38f21ddd8ea45b8dfedbc85cbc41
George-Honeywell <UP843814@myport.ac.uk>
2020-02-25 15:53:13 +0000

    Added Various things
    Added a true rng
    added a function to test function

--- RNG.cpp
+++ RNG.cpp
@@ -5,6 +5,7 @@
 #include <time.h>
 #include <cstdlib>
 #include <iomanip>
+#include <random>
 #define MAX_NUM 1000 // Define the maximum random number. Cannot be a decimal. To make a decimal, input two extra 00s and then after the execution divide by 100.
 
 using namespace std;
@@ -17,20 +18,36 @@ void RNG()
 
     for (int i = 0; i <= 10; i++)
     {
-        random = rand(); //% MAX_NUM;
+        random = rand() % MAX_NUM;
         cout << "First number: " << random << endl;
-        randomF = random / 100;
-        cout << "First random number in decimal: " << fixed <<setprecision(2) << randomF << endl; //This line includes the 'fixed...setprecision' line. This forces it to use 2 decimal places.
+        randomF = random / 100; // Divides random number to get two decimal places.
+        cout << "First random number in decimal: " << fixed << setprecision(2) << randomF << endl; //This line includes the 'fixed...setprecision' line. This forces it to use 2 decimal places.
        
     }
+}
+
+int func(int a, int b)
+{
+    return a * b / 10;
 }
 
 int main()
 {
     cout << "The random numbers are: " << endl;
-    RNG();
+    //RNG();
     cout << "That's it for the RNG, see you next time." << endl;
-    return 0;
+
+    //
+    //random_device rnd;
+    //cout << rnd.entropy() << endl;
+    //cout << rnd.min() << endl;
+    //cout << rnd.max() << endl;
+
+    
+    cout << func(98, 24);
+
+
+
 
 }
 
